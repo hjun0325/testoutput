5c3e4b8a0d29327bcb3618981c6edcdd287a7061
hjun0325 <100742383+hjun0325@users.noreply.github.com>
2022-05-04 10:51:20 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup (
+  include_package_data = True,
+  name = 'testoutput0325',
+  version = '2.0.1',
+  description = 'oss-dev test output 0325',
+  author = 'hongjunan',
+  author_email = 'km1707325@naver.com',
+  url = "https://github.com/hjun0325/testoutput",
+  install_requires = ['pytest'],
+  long_description = 'oss development',
+  long_description_content_type = 'text/markdown',
+  classifiers = [
+    "programming language :: python :: 3",
+    "operating system :: OS Independent",
+  ],
+)
